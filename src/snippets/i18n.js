474b15e945a979fec2274ae217d5c470fc4215e3
Copy Liu <copyliu@gmail.com>
2015-10-18 23:13:27 +0800

    i18n init

--- src/snippets/i18n.js
+++ src/snippets/i18n.js
@@ -18,7 +18,7 @@
 /*<<< require external //cdnjs.cloudflare.com/ajax/libs/i18next/1.10.2/i18next.min.js before >>>*/
 
 $(function() {
-    var i18noption = { resGetPath: 'locales/__lng__/__ns__.json',lowerCaseLng: true,lng:'zh-CN' };
+    var i18noption = { resGetPath: '/static/locales/__lng__/__ns__.json',lowerCaseLng: true,lng:'zh-CN' };
     i18n.init(i18noption,function(){
         $(body).i18n();
     });
